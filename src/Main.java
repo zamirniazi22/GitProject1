adebab81bead9f0270ffaf0804b484477f93659b
zniazi <zniazi265@gmail.com>
2022-12-17 10:59:00 -0500

    version2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println ("First version is completed");
+        System.out.println (" version is complete");
     }
 }
